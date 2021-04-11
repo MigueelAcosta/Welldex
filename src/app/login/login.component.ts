d458931b83b60c9d619c09c7cdc133a5b56797f7
Miguel Acosta <miguelacosta1294@outlook.com>
2021-04-11 00:49:35 -0500

    login validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserLoginModel } from '../models/user-login.model';
 import { HttpService } from '../service/http-service';
@@ -20,12 +20,23 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = this.formBuild.group({
-      user: new FormControl(),
-			password: new FormControl(),
+      user: new FormControl('',[Validators.required, Validators.minLength(4)]),
+			password: new FormControl('',[Validators.required, Validators.minLength(4)]),
     })
   }
 
+  get user () {
+    return this.form.get('user')
+  }
+
+  get password() {
+    return this.form.get('password')
+  }
+
   onSend(){
+    if(this.form.invalid){
+      return;
+    }
     let data: UserLoginModel = new UserLoginModel();
     data.user = this.form.get('user').value;
     data.password = this.form.get('password').value;
