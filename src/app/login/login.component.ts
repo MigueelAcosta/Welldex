a04e7eb11ffb0de8cee48c75694a84cc999ac9e6
Miguel Acosta <miguelacosta1294@outlook.com>
2021-04-10 22:08:18 -0500

    login component implementation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserLoginModel } from '../models/user-login.model';
+import { HttpService } from '../service/http-service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  constructor(private formBuild: FormBuilder,
+    private http:HttpService,
+    private router: Router) {
+
+   }
 
   ngOnInit(): void {
+    this.form = this.formBuild.group({
+      user: new FormControl(),
+			password: new FormControl(),
+    })
   }
 
+  onSend(){
+    let data: UserLoginModel = new UserLoginModel();
+    data.user = this.form.get('user').value;
+    data.password = this.form.get('password').value;
+    this.http.login(data).subscribe( response =>{
+      if(response.objectschemas.status == 'Ok'){
+        this.router.navigate(['/dashboard']);
+      }else{
+        alert('No se pudo iniciar sesión')
+      }
+    });
+  }
 }
